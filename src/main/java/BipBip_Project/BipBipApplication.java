cd88aed0ffaa503f4cdaae46eed2825d385bb1e0
labidkacem <labidkacem@gmail.com>
2025-02-15 11:32:30 +0100

    first commit

---
+++ src/main/java/BipBip_Project/BipBipApplication.java
@@ -0,0 +1,13 @@
+package BipBip_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BipBipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BipBipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BipBip_Project/BipBipApplicationTests.java
@@ -0,0 +1,13 @@
+package BipBip_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BipBipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
